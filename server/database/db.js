40be760c8030f70ef7204bee300d732701c6e4e3
Unnati <unnatisaxena1605@gmail.com>
2023-09-22 23:07:30 +0530

    changes made

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const Connection = async(password = '1234')=>{
-    const URL =`mongodb+srv://unnati:${password}@cluster0.v4hjypp.mongodb.net/?retryWrites=true&w=majority`;
+    const URL =`mongodb://unnati:unnati123@ac-zfpc8fk-shard-00-00.wppmub7.mongodb.net:27017,ac-zfpc8fk-shard-00-01.wppmub7.mongodb.net:27017,ac-zfpc8fk-shard-00-02.wppmub7.mongodb.net:27017/?ssl=true&replicaSet=atlas-q41g88-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
     try{
         await mongoose.connect(URL, {useUnifiedTopology: true, useNewUrlParser:true});
